f81a1f1d3622fc988e8aa3975928efaaca7df5b5
Ahsan <ahsanhabib462@gmail.com>
2024-02-13 10:45:10 +0600

    Stop Watch

--- viteProject/src/main.ts
+++ viteProject/src/main.ts
@@ -1,24 +1,114 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="Start" type="button">Start</button>
+      <p id="str"></p>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <div class="card">
+      <button id="Stop" type="button">Stop</button>
+      <p id="stop"></p>
+    </div>
+    <div class="card">
+      <button id="Duration" type="button">Duration</button>
+      <p id="duration"></p>
+    </div>
+    <div class="card">
+    <button id="Reset" type="button">Reset</button>
+    <p id="reset"></p>
+  </div>
   </div>
 `
+try {
+  class StopWatch {
+    private startTime: Date | null = null;
+    private endTime: number | null = null;
+    private duration: number = 0;
+    private isRunning: boolean = false;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+    start(): string {
+      if (this.isRunning) {
+        return `Already Running`;
+      }
+      this.startTime = new Date();
+      this.isRunning = true;
+      return `Started`;
+    }
+
+    stop(): string {
+      if (!this.isRunning) {
+        return `Not Running`;
+      }
+      this.endTime = new Date().getTime();
+      this.duration += (this.endTime - (this.startTime?.getTime() || 0)) / 1000;
+      this.startTime = null;
+      this.isRunning = false;
+      return `Stopped`;
+    }
+
+    reset(): void {
+      this.startTime = null;
+      this.endTime = null;
+      this.duration = 0;
+      this.isRunning = false;
+    }
+
+    getDuration(): number {
+      if (this.isRunning) {
+        this.endTime = new Date().getTime();
+        let cur = (this.endTime - (this.startTime?.getTime() || 0)) / 1000;
+        return this.duration+cur;
+      }
+      return this.duration;
+    }
+  }
+
+  let stopwatch = new StopWatch();
+
+  const startButton = document.querySelector<HTMLButtonElement>('#Start')!;
+  startButton.addEventListener('click', () => {
+    const status = stopwatch.start();
+    let statusElement = document.getElementById('str');
+    if (statusElement)
+      statusElement.textContent = status;
+    setTimeout(() => {
+      if (statusElement) statusElement.textContent = ''
+    }, 2000)
+  });
+  const stopButton = document.querySelector<HTMLButtonElement>('#Stop')!;
+  stopButton.addEventListener('click', () => {
+    const status = stopwatch.stop();
+    let statusElement = document.getElementById('stop');
+    if (statusElement)
+      statusElement.textContent = status;
+    setTimeout(() => {
+      if (statusElement) statusElement.textContent = ''
+    }, 2000)
+  });
+  const durationButton = document.querySelector<HTMLButtonElement>('#Duration')!;
+  durationButton.addEventListener('click', () => {
+    const status: number = stopwatch.getDuration();
+    let statusElement = document.getElementById('duration');
+    if (statusElement)
+      statusElement.textContent = String(status);
+    setTimeout(() => {
+      if (statusElement) statusElement.textContent = ''
+    }, 2000)
+  });
+  const ResetButton = document.querySelector<HTMLButtonElement>('#Reset')!;
+  ResetButton.addEventListener('click', () => {
+    stopwatch.reset();
+    let statusElement = document.getElementById('reset');
+    if (statusElement)
+      statusElement.textContent = 'Reseted';
+    setTimeout(() => {
+      if (statusElement) statusElement.textContent = ''
+    }, 2000)
+  });
+}
+catch (e) {
+  console.log(e)
+}
+
